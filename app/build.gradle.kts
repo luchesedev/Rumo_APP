86147f15ff917e192e8bc15835884ce0938c7184
Pedro augusto <pedroaugusto.borges36@gmail.com>
2026-06-10 18:21:04 -0300

    Adicionando a função do pdf e ajustando bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 // OkHttp (logs de requisição)
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.itextpdf:itext7-core:7.2.5")
 }
